53afdd98f11c90cc1b868158c0a266fdb8d6b000
eric <fantasy_2003_@hotmail.com>
2021-11-06 20:56:09 +0800

    add jacob

---
+++ modified/laplace2d/laplace2d.cpp
@@ -0,0 +1,122 @@
+#include <math.h>
+#include <string.h>
+#include "timer.h"
+#define nx 4096
+#define _A(array, ix, iy) (array[(ix) + nx * (iy)])
+#define real double
+
+int fun( int i, int j )
+{
+    int result = i + nx * j;
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    int m = 4096;
+    int n = 4096;
+    int iter_max = 1000;
+    
+    const float pi  = 2.0f * asinf(1.0f);
+    const float tol = 1.0e-5f;
+    float error     = 1.0f;
+	
+	long mn = m * n;
+    
+	real * A = (real*)malloc(mn*sizeof(real));
+	real * Anew = (real*)malloc(mn*sizeof(real));
+	real * y0 = (real*)malloc(n*sizeof(real));
+
+    //memset(A, 0, mn * sizeof(real));
+
+    for ( int j = 0; j < n; j++ )
+    {
+        for ( int i = 0; i < m; i++ )
+        {
+            _A(A, i, j) = 0.0f;
+        }
+    }
+    
+    // set boundary conditions
+    for (int i = 0; i < m; i++)
+    {
+		_A(A, i, 0) = 0.0f;
+		_A(A, i, n-1) = 0.0f;
+        //int id = fun( i, n-1);
+        //real z = A[id];
+        //int kkk = 1;
+    }
+    
+    for (int j = 0; j < n; j++)
+    {
+        y0[j] = sinf(pi * j / (n-1));
+		_A(A, 0, j) = y0[j];
+		_A(A, m-1, j) = y0[j]*expf(-pi);
+    }
+    
+    printf("Jacobi relaxation Calculation: %d x %d mesh\n", n, m);
+	
+    
+    StartTimer();
+    int iter = 0;
+    
+//#pragma omp parallel for shared(Anew)
+    for (int i = 1; i < m; i++)
+    {
+		_A(Anew, i, 0) = 0.f;
+		_A(Anew, i, n-1) = 0.f;
+    }	
+//#pragma omp parallel for shared(Anew)    
+    for (int j = 1; j < n; j++)
+    {
+		_A(Anew, 0, j) = y0[j];
+		_A(Anew, m-1, j) = y0[j]*expf(-pi);
+    }	
+    
+    while ( error > tol && iter < iter_max )
+    {
+        error = 0.f;
+
+//#pragma omp parallel for shared(mn, Anew, A)
+//#pragma acc kernels
+        for( int j = 1; j < n-1; j++)
+        {
+            for( int i = 1; i < m-1; i++ )
+            {
+                //Anew[j][i] = 0.25f * ( A[j][i+1] + A[j][i-1]
+                //                     + A[j-1][i] + A[j+1][i]);
+				real a1 = _A(A, i - 1, j);
+				real a2 = _A(A, i + 1, j);
+				real a3 = _A(A, i, j + 1);
+                real a4 = _A(A, i, j - 1);
+				real result = 0.25 * ( a1 + a2 + a3 + a4 );
+				real a0 = _A(A, i, j);
+				_A(Anew, i, j) = result;
+                error = fmaxf( error, fabsf( result - a0 ));
+            }
+        }
+        
+//#pragma omp parallel for shared(mn, Anew, A)
+//#pragma acc kernels
+        for( int j = 1; j < n-1; j++)
+        {
+            for( int i = 1; i < m-1; i++ )
+            {
+                //A[j][i] = Anew[j][i];    
+				_A(A, i, j) = _A(Anew, i, j);
+            }
+        }
+
+        if(iter % 100 == 0) printf("%5d, %0.6f\n", iter, error);
+        
+        iter++;
+    }
+
+    double runtime = GetTimer();
+ 
+    printf(" total: %f s\n", runtime / 1000.f);
+	
+	free(y0);
+	free(A);
+	free(Anew);	
+}
---
+++ modified/laplace2d_v6/laplace2d.cpp
@@ -0,0 +1,122 @@
+/*
+ *  Copyright 2015 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include "timer.h"
+
+//#define DOUBLE
+#ifdef DOUBLE
+	typedef double Real;
+	#define MY_EXP exp
+	#define MY_ASIN asin
+	#define MY_FMAX fmax
+	#define MY_ABS fabs
+#else
+	typedef float Real;	
+	#define MY_EXP expf
+	#define MY_ASIN asinf
+	#define MY_FMAX fmaxf
+	#define MY_ABS fabsf
+#endif
+
+
+int main(int argc, char** argv)
+{
+    int n = 4096;
+    int m = 4096;
+    int iter_max = 1000;
+    
+    const Real pi  = 2.0f * MY_ASIN(1.0f);
+    const Real tol = 1.0e-5f;
+    Real error     = 1.0f;
+    
+    Real *restrict A = (Real*)malloc(sizeof(Real)*n*m);
+    Real *restrict Anew = (Real*)malloc(sizeof(Real)*n*m);
+    Real *restrict y0 = (Real*)malloc(sizeof(Real)*n);
+
+    memset(A, 0, n * m * sizeof(Real));
+    
+    // set boundary conditions
+    for (int i = 0; i < m; i++)
+    {
+        A[0*m+i]   = 0.f;
+        A[(n-1)*m+i] = 0.f;
+    }
+    
+    for (int j = 0; j < n; j++)
+    {
+        y0[j] = sin(pi * j / (n-1));
+        A[j*m+0] = y0[j];
+        A[j*m+m-1] = y0[j]*MY_EXP(-pi);
+    }
+    
+    printf("Jacobi relaxation Calculation: %d x %d mesh\n", n, m);
+    
+    StartTimer();
+    int iter = 0;
+    
+#pragma omp parallel for shared(Anew)
+    for (int i = 1; i < m; i++)
+    {
+       Anew[0*m+i]   = 0.0;
+       Anew[(n-1)*m+i] = 0.0;
+    }
+#pragma omp parallel for shared(Anew)    
+    for (int j = 1; j < n; j++)
+    {
+        Anew[j*m+0]   = y0[j];
+        Anew[j*m+m-1] = y0[j]*MY_EXP(-pi);
+    }
+    
+    while ( error > tol && iter < iter_max ) {
+        error = 0.f;
+
+    #pragma acc kernels 
+    {
+        #pragma acc loop independent
+        for( int j = 1; j < n-1; j++) {
+            for( int i = 1; i < m-1; i++ ) {
+
+                Anew[j*m+i] = 0.25f * ( A[j*m+i+1] + A[j*m+i-1]
+                                     + A[(j-1)*m+i] + A[(j+1)*m+i]);
+
+                error = MY_FMAX( error, MY_ABS(Anew[j*m+i]-A[j*m+i]));
+            }
+        }
+
+        #pragma acc loop independent
+        for( int j = 1; j < n-1; j++) {
+            for( int i = 1; i < m-1; i++ ) {
+                A[j*m+i] = Anew[j*m+i];    
+            }
+        }
+    }
+
+        if(iter % 100 == 0) printf("%5d, %0.6f\n", iter, error);
+        
+        iter++;
+    }
+
+    double runtime = GetTimer();
+ 
+    printf(" total: %f s\n", runtime / 1000.f);
+	
+	free(A);
+	free(Anew);
+	free(y0);
+}
